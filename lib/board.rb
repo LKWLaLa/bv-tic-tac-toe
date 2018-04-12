2fb0b4846d9f5ae98bc8b5350ee94d89954a0456
LKWLaLa <lkwlala@gmail.com>
2018-04-12 18:41:58 -0400

    add Board class

---
+++ lib/board.rb
@@ -0,0 +1,37 @@
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    @cells = Array.new(9, " ")
+  end  
+
+  def reset!
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-" * 11
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-" * 11
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
+
+  def update(location, token)
+    cells[location.to_i] = token
+  end
+
+  def taken?(location)
+    cells[location.to_i] != " "    
+  end
+
+  def valid_move?(location)
+    location.to_i.between?(0,8) && !taken?(location)
+  end
+
+  def full?
+    !cells.include?(" ")
+  end
+
+end
