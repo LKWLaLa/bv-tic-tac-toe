99b2fd201a356728076cdcfea925ed50c858a63a
LKWLaLa <lkwlala@gmail.com>
2018-04-13 20:40:19 -0400

    update range of computer selection

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,11 +2,11 @@ require_relative './player.rb'
 
 class Computer < Player
 
-#will default to trying the center first, followed by a stupid, random choice
+#will default to trying the upper left corner, followed by a stupid, random choice
   def choose_location(board)
-    location = 4
+    location = 0
     until board.valid_move?(location)
-      location = rand(0..8)
+      location = rand(1..board.row_length - 1)
     end
     location
   end
